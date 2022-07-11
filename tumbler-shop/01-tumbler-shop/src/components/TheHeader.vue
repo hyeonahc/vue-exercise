2aec323e18c13e6cca02938fc3606d6118614a18
hyeonah <hyeonah.hello@gmail.com>
2022-07-11 15:18:32 +0900

    [01-tumbler-shop] feat: Add login status logic

--- tumbler-shop/01-tumbler-shop/src/components/TheHeader.vue
+++ tumbler-shop/01-tumbler-shop/src/components/TheHeader.vue
@@ -1,26 +1,39 @@
 <template>
   <header>
+    <!-- <RouterLink :to="{name: 'mainpage'}" -->
     <RouterLink to="/">
       MainPage
     </RouterLink>
-    <RouterLink to="/signup">
-      SignUp
-    </RouterLink>
-    <RouterLink to="/signin">
-      SignIn
-    </RouterLink>
-    <RouterLink to="/mypage">
-      MyPage
-    </RouterLink>
-    <RouterLink to="/">
-      SignOut
-    </RouterLink>
+    <span v-if="!isLogin">
+      <RouterLink to="/signup">
+        SignUp
+      </RouterLink>
+      <RouterLink to="/signin">
+        SignIn
+      </RouterLink>
+    </span>
+    <span v-else>
+      <RouterLink to="/mypage">
+        MyPage
+      </RouterLink>
+      <RouterLink to="/">
+        SignOut
+      </RouterLink>
+    </span>
   </header>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+// mapState: store에 저장된 state를 배열로 등록해주는 함수
+// mapState('스토어이름', [state에 등록한 데이터이름...])
+// mapActions: store에 저장된 actions를 배열로 등록해주는 함수
+
 export default {
-  
+  // 스토어에 등록한 state는 컴포넌트에서 사용시 computed로 액세스한다
+  computed: {
+    ...mapState('user', ['isLogIn'])
+  },
 }
 </script>
 
